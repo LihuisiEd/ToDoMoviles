ed28ea6ede4fa97d66bd7f2c04fabe78b113a53a
romulo huaman cusi <romulohuaman@tecsup.edu.pe>
2023-07-01 12:23:57 -0500

    inicio de sesion terminado

--- ToDoMoviles/Controladores/ViewController.swift
+++ ToDoMoviles/Controladores/ViewController.swift
@@ -6,17 +6,35 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     @IBAction func btnLogin(_ sender: Any) {
+        Auth.auth().signIn(withEmail: txtEmail.text!, password: txtPassword.text!) { (user, error) in
+            print("Intentando Iniciar Sesión")
+            if error != nil{
+                print("Se presentó el siguiente error \(error)")
+                let alerta = UIAlertController(title: "Creación de Usuario", message: "Usuario no registrado, ¿desea registrarlo?", preferredStyle: .alert)
+                let btnOK = UIAlertAction(title: "Crear", style: .default) { (UIAlertAction) in
+                    self.performSegue(withIdentifier: "registroSegue", sender: nil)
+                    }
+                let btnCANCEL = UIAlertAction(title: "Cancelar", style: .cancel) { (UIAlertAction) in
+                    alerta.dismiss(animated: true, completion: nil)
+                    }
+                alerta.addAction(btnOK)
+                alerta.addAction(btnCANCEL)
+                self.present(alerta, animated: true, completion: nil)
+            }else{
+                print("Inicio de sesión exitoso")
+                self.performSegue(withIdentifier: "iniciarsesionSegue", sender: nil)
+            }
+        }
     }
     @IBAction func btnRegister(_ sender: Any) {
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
