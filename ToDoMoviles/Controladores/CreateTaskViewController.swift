bf6b0fd1bd9b2dfb4cf5784fe6b5656206d2892f
Angellvz <luis.ventura@tecsup.edu.pe>
2023-07-05 12:09:37 -0500

    put

--- ToDoMoviles/Controladores/CreateTaskViewController.swift
+++ ToDoMoviles/Controladores/CreateTaskViewController.swift
@@ -55,25 +55,48 @@ class CreateTaskViewController: UIViewController {
     }
 
     @IBAction func agregarTarea(_ sender: Any) {
-        if txtTitulo.text != nil{
-            let selectedDate = self.txtFecha.date
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "dd-mm-yyyy HH:mm:ss"
-            let dateString = dateFormatter.string(from: selectedDate)
-            let tareaData: [String: Any] = [
-                "titulo": self.txtTitulo ?? "",
-                "contenido": self.txtContenido ?? "",
-                "fecha": dateString
-            ]
-            let tareaRef = self.database.child("tareas").childByAutoId()
-            tareaRef.setValue(tareaData)
-            self.navigationController?.popViewController(animated: true)
-        }
+        if let titulo = txtTitulo.text, let contenido = txtContenido.text {
+                let selectedDate = self.txtFecha.date
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
+                let dateString = dateFormatter.string(from: selectedDate)
+                
+                let tareaData: [String: Any] = [
+                    "titulo": titulo,
+                    "contenido": contenido,
+                    "fecha": dateString
+                ]
+                
+            let tareaRef = self.database.child("usuarios").child("1").child("tareas").childByAutoId()
+                tareaRef.setValue(tareaData) { error, _ in
+                    if let error = error {
+                        print("Error uploading task to Firebase: \(error.localizedDescription)")
+                    } else {
+                        print("Task uploaded successfully to Firebase")
+                        self.navigationController?.popViewController(animated: true)
+                    }
+                }
+            }
     }
     
     
     @IBAction func putTarea(_ sender: Any) {
-        
+        if let tareaId = tareaPut?.id {
+                let updatedData: [String: Any] = [
+                    "titulo": "Nuevo título",
+                    "contenido": "Nuevo contenido",
+                    "fecha": "Nueva fecha"
+                ]
+                
+                let tareaRef = self.database.child("tareas").child(tareaId)
+                tareaRef.setValue(updatedData) { (error, _) in
+                    if let error = error {
+                        print("Error updating task in Firebase: \(error.localizedDescription)")
+                    } else {
+                        print("Task updated successfully in Firebase")
+                    }
+                }
+            }
     }
     
 
